f72241802ae47fd30fb3a9f317d2411f10f484df
Victoria Qian <Qian@qianmatombp.wireless.newschool.edu>
2019-04-01 21:30:30 -0400

    API Update

--- Assignment 8/README.md
+++ Assignment 8/README.md
@@ -4,4 +4,6 @@
 2. The temperature decides the size of the background, from the lowest temperature of the day to the highest.
 3. The position of sunrise and sunset is scaled, comparing the sunrise/sunset time to a 24-hour day scheme.
 
+* It is one day late. I think it is because the average day of month (from January to now) is smaller than the average day of month over a year (30.44).
+
 ![Demo](milanApi.gif)
--- Assignment 8/src/ofApp.cpp
+++ Assignment 8/src/ofApp.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 float humidity;
 int currentYear, currentMonth, currentDate;
-float currentM;
+float currentM,currentD;
 int year, month, day;
 int epochtime;
 float a, b;
@@ -22,8 +22,8 @@ void ofApp::setup(){
     } else {
         ofLogNotice("ofApp::setup") << "Failed to parse JSON.";
     }
-    epochtime = json["daily"]["data"][1]["time"].asInt();
     
+    epochtime = json["daily"]["data"][0]["time"].asInt();
     sunR=json["daily"]["data"][0]["sunriseTime"].asFloat();
     sunS=json["daily"]["data"][0]["sunsetTime"].asFloat();
     current=json["daily"]["data"][0]["time"].asFloat();
@@ -39,18 +39,25 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    year = 60*60*24*365;
+    year = 60*60*24*365.24;
     currentYear = 1970 + epochtime/year;
     cout<<"currentYear"<<endl;
     month = 60*60*24*30.44;
     currentMonth = (epochtime % year)/month;
-    currentM =(epochtime % year)/month;
+    currentM =(epochtime % year) % month;
     cout<<"currentMonth"<<endl;
+    if (currentM > 0){
+        currentMonth ++;
+    } // When there is reminder, it is already next month.
     day = 60*60*24;
     currentDate = ((epochtime % year) % month)/ day;
+    currentD = ((epochtime % year) % month) % day;
     cout<<"currentDate"<<endl;
+    if (currentD > 0){
+        currentDate ++;
+    } // The json["daily"]["data"][0]["time"].asInt() starts at 12am, no reminder, but it is the next day, plus 1.
     
-//    ofMap(10, 0, ofGetWidth(), json["daily"]["data"][0]["temperatureLow"].asFloat(), json["daily"]["data"][0]["temperatureHigh"].asFloat())
+
     tempHigh=json["daily"]["data"][0]["temperatureHigh"].asFloat();
     tempLow=json["daily"]["data"][0]["temperatureLow"].asFloat();
     tempRatio= (temperature-tempLow)/(tempHigh-tempLow);
@@ -87,9 +94,44 @@ void ofApp::draw(){
     std::string low  = json["daily"]["data"][0]["temperatureLow"].asString();
     std::string uvIndex  = json["daily"]["data"][0]["uvIndex"].asString();
     std::string summary = json["daily"]["data"][0]["summary"].asString();
-    ofDrawBitmapString(currentYear, 120, 90);
-    ofDrawBitmapString(currentMonth, 160, 90);
-    ofDrawBitmapString(currentDate, 175, 90);
+    if (currentMonth==1) {
+        ofDrawBitmapString("Jan.", 120, 90);
+    }
+    if (currentMonth==2) {
+        ofDrawBitmapString("Feb.", 120, 90);
+    }
+    if (currentMonth==3) {
+        ofDrawBitmapString("Mar.", 120, 90);
+    }
+    if (currentMonth==4) {
+        ofDrawBitmapString("Apr.", 120, 90);
+    }
+    if (currentMonth==5) {
+        ofDrawBitmapString("May.", 120, 90);
+    }
+    if (currentMonth==6) {
+        ofDrawBitmapString("Jun.", 120, 90);
+    }
+    if (currentMonth==7) {
+        ofDrawBitmapString("Jul.", 120, 90);
+    }
+    if (currentMonth==8) {
+        ofDrawBitmapString("Aug.", 120, 90);
+    }
+    if (currentMonth==9) {
+        ofDrawBitmapString("Sept.", 120, 90);
+    }
+    if (currentMonth==10) {
+        ofDrawBitmapString("Oct.", 120, 90);
+    }
+    if (currentMonth==11) {
+        ofDrawBitmapString("Nov.", 120, 90);
+    }
+    if (currentMonth==12) {
+        ofDrawBitmapString("Dec.", 120, 90);
+    }
+    ofDrawBitmapString(currentDate, 155, 90);
+    ofDrawBitmapString(currentYear, 180, 90);
     ofDrawBitmapString("Milan, Italy", 120, 120);
     ofDrawBitmapString("current epoch time", 120, 160);
     ofDrawBitmapString(time, 120, 180);
@@ -104,7 +146,7 @@ void ofApp::draw(){
     ofDrawBitmapString("uvIndex", 120, 410);
     ofDrawBitmapString(uvIndex, 120, 430);
     ofDrawBitmapString(summary, 120, 460);
-//    ofDrawBitmapString(a, 150, 500);
+    ofDrawBitmapString(currentM, 150, 500);
     
     ofSetColor(246, 218, 102);
     ofSetCircleResolution(40);
